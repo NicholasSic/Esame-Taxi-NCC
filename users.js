797a52852e5ce87346faf6e4a90645dd0b75c935
NicholasSic <nikyfat@gmail.com>
2025-09-24 10:23:50 +0200

    TEST

--- users.js
+++ users.js
@@ -1,11 +1,12 @@
 window.USERS = [
-{
-  "email": "nikyfat@gmail.com",
-  "salt": "2f1ddaa0119150263a3006cd469604ee",
-  "hash": "2fab848b09fee809115394eaaa0a443482405e7aa05394ceefdb86fde6ea0530"
-},
-{
-  "email": "test@example.com",
-  "salt": "a1b2c3d4e5f60718293a4b5c6d7e8f90",
-  "hash": "7d2d12593e996659171151a56936c16fb7210a8b34649cbaf9f8ef08b5398280" }
+  {
+    email: "test@example.com",
+    salt: "5f09cc4d34ced414cd94c657d6a76926",
+    hash: "41b0273f8c8f412d200f9a1c1a98f5c0bb8b7b7c3af6b320e267de17a664fa84"
+  },
+  {
+  "email": "login@mail.com",
+  "salt": "0ff0c73019828ea110abf4f26b305d6e",
+  "hash": "d84fd2614b17dde9d56ca050079f591d816e335b5efc8554ca3403c29e94c1f0"
+}
 ];
