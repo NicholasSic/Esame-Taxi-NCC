94be1a00d3300e45efa3901cf27941f7a687c88d
NicholasSic <nikyfat@gmail.com>
2025-09-24 12:04:34 +0200

    Versione Finale con Login

--- users.js
+++ users.js
@@ -4,9 +4,9 @@ window.USERS = [
     salt: "5f09cc4d34ced414cd94c657d6a76926",
     hash: "41b0273f8c8f412d200f9a1c1a98f5c0bb8b7b7c3af6b320e267de17a664fa84"
   },
-  {
-  "email": "login@mail.com",
-  "salt": "0ff0c73019828ea110abf4f26b305d6e",
-  "hash": "d84fd2614b17dde9d56ca050079f591d816e335b5efc8554ca3403c29e94c1f0"
+ {
+  "email": "nikyfat@gmail.com",
+  "salt": "8df3b47fae160fffc0ee82b60249e21e",
+  "hash": "d98a84d2ad2a9e2ac4f752750ab8470c74e0d96d9855544f552d2c27ebe091c9"
 }
 ];
