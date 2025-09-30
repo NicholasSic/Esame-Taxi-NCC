e55d127091b0b366fdadec960b21e787697b91ee
NicholasSic <nikyfat@gmail.com>
2025-09-30 10:16:54 +0200

    Add files via upload

--- users.js
+++ users.js
@@ -4,9 +4,24 @@ window.USERS = [
     salt: "5f09cc4d34ced414cd94c657d6a76926",
     hash: "41b0273f8c8f412d200f9a1c1a98f5c0bb8b7b7c3af6b320e267de17a664fa84"
   },
+    {
+    email: "Davide Brioschi",
+    salt: "01a6d2af3262708afa1ff067a4ad21f9",
+    hash: "aeeae99a80773bb37cd92d4411337d24ca13557e1280a6b4fd078b8e4528cb02"
+  },  
+  {
+    email: "Davide Di Ronza",
+    salt: "8fecd54dc174c4e65980805e304788e5",
+    hash: "0e809ce5d541ca0c5062a9a4a9348994018c5d4932e26e5c71834f03e8b506b2"
+  },  
+  {
+    email: "Christian Pappagallo",
+    salt: "d9d23915d01c390046d30821e8543dad",
+    hash: "970e7727bd8c7d331d07c4bbde663942c6b629f99bf1ab1a96b470163adc5f06"
+  },
  {
-  "email": "nikyfat@gmail.com",
-  "salt": "8df3b47fae160fffc0ee82b60249e21e",
-  "hash": "d98a84d2ad2a9e2ac4f752750ab8470c74e0d96d9855544f552d2c27ebe091c9"
+  email: "nikyfat@gmail.com",
+  salt: "8df3b47fae160fffc0ee82b60249e21e",
+  hash: "d98a84d2ad2a9e2ac4f752750ab8470c74e0d96d9855544f552d2c27ebe091c9"
 }
 ];
