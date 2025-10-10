fdfc1df28e233c6b2e7965e3ed553c8788e475b6
NicholasSic <nikyfat@gmail.com>
2025-10-10 12:37:39 +0200

    Add files via upload

---
+++ confluenze.js
@@ -0,0 +1,59 @@
+window.CONFLUENZE = [
+  {
+    piazza: "Piazzale Loreto",
+    strade: [
+      "Corso Buenos Aires",
+      "Viale Abruzzi",
+      "Via Porpora",
+      "Via Andrea Costa",
+      "Via Padova",
+      "Viale Monza",
+      "Viale Brianza",
+      "Via Andrea Doria"
+    ],
+    alias: {
+      "Buenos Aires": "Corso Buenos Aires",
+    }
+  },
+  {
+    piazza: "Piazzale Cadorna",
+    strade: [
+      "Foro Buonaparte Dispari",
+      "Via Minghetti",
+      "Via Paleocapa",
+      "Via Boccaccio",
+      "Via Carducci",
+      "Via San Nicolao"
+    ],
+    alias: {
+      "Foro Buonaparte": "Foro Buonaparte Dispari",
+    }
+  },
+  {
+    piazza: "Piazza Cavour",
+    strade: [
+      "Via Manzoni",
+      "Via Senato",
+      "Via del Vecchio Politecnico",
+      "Via Palestro",
+      "Via Manin",
+      "Via Turati",
+      "Via Fatebenefratelli"
+    ],
+  },
+    {
+    piazza: "Piazza 24 Maggio",
+    strade: [
+      "Corso di San Gottardo",
+      "Via Trincea delle Frasche",
+      "Via Col di Lana",
+      "Via Gian Galeazzo",
+      "Via Sambuco",
+      "Corso di Porta Ticinese",
+      "Via Gabriele D'Annunzio",
+      "Via Gorizia",
+      "Via Scoglio di Quarto",
+      "Via Manusardi"
+    ],
+  },
+];
